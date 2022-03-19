062331ada3d3fbdc5949dd8b8f97f9dd7f365d7e
RayMightBeWrong <ruizinho7braga@gmail.com>
2022-03-19 17:04:39 +0000

    Organizar main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-
 import sys
 import re
 import csv
-import genero_por_ano
+import distr_morada, genero_por_ano
 
 # cabeçalho _id, index, dataEMD, nome/primeiro, nome/último, idade, género, morada, modalidade, clube, email, federado, resultado
 # separador: ','
@@ -28,13 +27,11 @@ def init_html(filename):
     </body>
 </html>''')
 
-#Função principal que vai correr todas as funcionalidades e para cada uma criar o respetivo html e depois criar index.html como link geral para todas
-def main():
+def initDict():
     f = open('emd.csv', mode = 'r')
     f = csv.reader(f)
-    regex_emd = r"(?P<Id>\d{7}[A-Za-z0-9]{17}),(?P<Index>\d\d?),(?P<Data>\d{4}-\d{2}-\d{2}),(?P<Primeironome>[A-Z][a-z]+),(?P<Ultimonome>[A-Z][a-z]+),(?P<Idade>\d{1,2}),(?P<Genero>(M|F)),(?P<Morada>[A-Z][a-z]+),(?P<Modalidade>[A-Z][A-Za-zçãé]+),(?P<Clube>[A-Z][A-Za-zã]+),(?P<Email>[A-Za-z.]+[A-Za-z]+@[A-Za-z.ã]+[A-Za-z]),(?P<Federado>true|false),(?P<Resultado>true|false)"
+    regex_emd = r'(?P<Id>\d{7}[A-Za-z0-9]{17}),(?P<Index>\d\d?),(?P<Data>\d{4}-\d{2}-\d{2}),(?P<Primeironome>[A-Z][a-z]+),(?P<Ultimonome>[A-Z][a-z]+),(?P<Idade>\d{1,2}),(?P<Genero>(M|F)),(?P<Morada>[A-Z][a-z]+),(?P<Modalidade>[A-Z][A-Za-zçãé]+),(?P<Clube>[A-Z][A-Za-zã]+),(?P<Email>[A-Za-z.]+[A-Za-z]+@[A-Za-z.ã]+[A-Za-z]),(?P<Federado>true|false),(?P<Resultado>true|false)'
     p_emd     = re.compile(regex_emd)
-    print(regex_emd)
     atletas = []
     for line in f:
         l = ','.join(line)
@@ -42,7 +39,13 @@ def main():
         if m_emd:
             dict = m_emd.groupdict()
             atletas.append(dict)
-    print(atletas)
+    return atletas
+
+
+#Função principal que vai correr todas as funcionalidades e para cada uma criar o respetivo html e depois criar index.html como link geral para todas
+def main():
+    atletas = initDict()
     init_html("index.html")
     
 main()
+
