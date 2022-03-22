d667db32934645ba6d8aaedac2ec476d215ef8a8
davidao06 <a93274@alunos.uminho.pt>
2022-03-22 22:39:17 +0000

    Adicionar paginas dos indicadores com tabelas html

--- querys/aptos_por_ano.py
+++ querys/aptos_por_ano.py
@@ -4,12 +4,67 @@ trueRegex = r'[Tt][Rr][Uu][Ee]'
 def orderNome(e):
     return e['Primeironome'] + ' ' + e['Ultimonome']
 
+def html_info_utilizada(tuplo):
+    f_html = open("html_code/distro_aptos_por_anos.html","w")
+    f_html.write('''<!doctype html>
+<html>
+  <head>
+    <title>Informação utilizada para calcular distribuição dos atletas federados por ano</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+  <h1> Informação utilizada para calcular distribuição dos atletas federados por ano</h1>\n<br>\n''')
+    dicAptoAno = tuplo[0]
+    dicNAptoAno = tuplo[1]
+    for ano in sorted(dicAptoAno.keys()):
+        f_html.write("<br>\n")
+        f_html.write(f'''
+            <table>
+            <caption> <b>Atletas considerados aptos no ano {ano}</b> <caption>
+                <tr>
+                    <th>Nome do atleta</th>
+                    <th>Data do exame médico</th>
+                    <th>Modalidade </th>
+                    <th>Clube </th>
+                </tr>''')
+        for atleta in dicAptoAno[ano]:
+            f_html.write(f'''
+                <tr>
+                    <td>{atleta['Primeironome']} {atleta['Ultimonome']} </td>
+                    <td>{atleta['Data']} </td>
+                    <td>{atleta['Modalidade']}</td>
+                    <td>{atleta['Clube']}</td>
+                </tr>\n''' )
+        f_html.write('            </table>')
+        f_html.write("<br>\n")
+        f_html.write(f'''
+            <table>
+            <caption> <b>Atletas federados no ano {ano}</b> <caption>
+                <tr>
+                    <th>Nome do atleta</th>
+                    <th>Data do exame médico</th>
+                    <th>Modalidade </th>
+                    <th>Clube </th>
+                </tr>''')
+        for atleta in dicNAptoAno[ano]:
+            f_html.write(f'''
+                <tr>
+                    <td>{atleta['Primeironome']} {atleta['Ultimonome']} </td>
+                    <td>{atleta['Data']} </td>
+                    <td>{atleta['Modalidade']}</td>
+                    <td>{atleta['Clube']}</td>
+                </tr>\n''' )
+        f_html.write('            </table>')
+    f_html.write('''    <br> <a href="index.html">Voltar à pagina inicial aqui</a> <br>
+  </body>
+</html>''')
+
 def aptos_por_ano_to_index(tuplo):
     dicAptosAno = tuplo[0]
     dicNAptosAno = tuplo[1]
     index = open('html_code/index.html','a')
     index.write('''        <h2> &nbsp;&nbspPercentagem de aptos e não aptos por ano </h2>\n''')
-    for ano in dicAptosAno.keys():
+    for ano in sorted(dicAptosAno.keys()):
         numeroAtletasAptos =len(dicAptosAno[ano])
         numeroAtletasNAptos = len(dicNAptosAno[ano])
         numeroAtletasAno = numeroAtletasAptos + numeroAtletasNAptos
@@ -18,7 +73,7 @@ def aptos_por_ano_to_index(tuplo):
         index.write(f'          <p>  &nbsp;&nbsp &nbsp;&nbsp &nbsp;&nbsp;No ano de {ano} realizaram-se {numeroAtletasAno} exames médicos a atletas\n')
         index.write(f'          <p>  &nbsp;&nbsp &nbsp;&nbsp &nbsp;&nbsp;Desses {numeroAtletasAno}, {numeroAtletasAptos} apresentaram exames positivos enquanto {numeroAtletasNAptos} não estão aptos a competir</p>\n')
         index.write(f'          <p>  &nbsp;&nbsp &nbsp;&nbsp &nbsp;&nbsp;Percentagem dos aptos : {percentagemAptosAno}; Percentagem dos não aptos : {percentagemNAptosAno}</p>\n')
-    index.write(f'        <a href="distro_federado_por_anos.html">Mais informação aqui</a> <br>\n')  
+    index.write(f'        <a href="distro_aptos_por_anos.html">Mais informação aqui</a> <br>\n')  
     index.close()
     
 
@@ -40,4 +95,5 @@ def aptos_por_ano(atletas):
     for key in sorted(dicAptoAno.keys()):
         dicAptoAno[key].sort(key = orderNome)
         dicNAptoAno[key].sort(key = orderNome)
+    html_info_utilizada((dicAptoAno,dicNAptoAno))
     return (dicAptoAno,dicNAptoAno)    
--- querys/federado_por_ano.py
+++ querys/federado_por_ano.py
@@ -4,12 +4,67 @@ trueRegex = r'[Tt][Rr][Uu][Ee]'
 def orderNome(e):
     return e['Primeironome'] + ' ' + e['Ultimonome']
 
+def html_info_utilizada(tuplo):
+    f_html = open("html_code/distro_federado_por_anos.html","w")
+    f_html.write('''<!doctype html>
+<html>
+  <head>
+    <title>Informação utilizada para calcular distribuição dos atletas federados por ano</title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+  <h1> Informação utilizada para calcular distribuição dos atletas federados por ano</h1>\n''')
+    dicFedAno = tuplo[0]
+    dicNFedAno = tuplo[1]
+    for ano in sorted(dicFedAno.keys()):
+        f_html.write("<br>\n")
+        f_html.write(f'''
+            <table>
+            <caption> <b>Atletas federados no ano {ano}</b> <caption>
+                <tr>
+                    <th>Nome do atleta</th>
+                    <th>Data do exame médico</th>
+                    <th>Modalidade </th>
+                    <th>Clube </th>
+                </tr>''')
+        for atleta in dicFedAno[ano]:
+            f_html.write(f'''
+                <tr>
+                    <td>{atleta['Primeironome']} {atleta['Ultimonome']} </td>
+                    <td>{atleta['Data']} </td>
+                    <td>{atleta['Modalidade']}</td>
+                    <td>{atleta['Clube']}</td>
+                </tr>\n''' )
+        f_html.write('            </table>')
+        f_html.write("<br>\n")
+        f_html.write(f'''
+            <table>
+            <caption> <b>Atletas não federados no ano {ano}</b> <caption>
+                <tr>
+                    <th>Nome do atleta</th>
+                    <th>Data do exame médico</th>
+                    <th>Modalidade </th>
+                    <th>Clube </th>
+                </tr>''')
+        for atleta in dicNFedAno[ano]:
+            f_html.write(f'''
+                <tr>
+                    <td>{atleta['Primeironome']} {atleta['Ultimonome']} </td>
+                    <td>{atleta['Data']} </td>
+                    <td>{atleta['Modalidade']}</td>
+                    <td>{atleta['Clube']}</td>
+                </tr>\n''' )
+        f_html.write('            </table>')        
+    f_html.write('''    <br> <a href="index.html">Voltar à pagina inicial aqui</a> <br>
+  </body>
+</html>''')
+
 def federado_por_ano_to_index(tuplo):
     dicFed = tuplo[0]
     dicNFed = tuplo[1]
     index = open('html_code/index.html','a')
     index.write('''        <h2> &nbsp;&nbspDistribuição por estatuto de federado em cada ano </h2>\n''')
-    for ano in dicFed.keys():
+    for ano in sorted(dicFed.keys()):
         numeroAtletasFed =len(dicFed[ano])
         numeroAtletasNFed = len(dicNFed[ano])
         numeroAtletasAno = numeroAtletasFed + numeroAtletasNFed
@@ -42,5 +97,6 @@ def federado_por_ano(atletas):
     for key in sorted(dicFedAno.keys()):
         dicFedAno[key].sort(key = orderNome)
         dicNFedAno[key].sort(key = orderNome)
+    html_info_utilizada((dicFedAno,dicNFedAno))
     return (dicFedAno,dicNFedAno)
         
